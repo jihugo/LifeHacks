2bb26d2f0831c5dd2d7e4c8b3e2538fc84e7a5a2
jihugo <yugeji2@illinois.edu>
2023-03-08 18:45:50 -0600

    add consideration for edge cases

--- tools/convolution.py
+++ tools/convolution.py
@@ -1,15 +1,32 @@
+"""Some useful python fucntions"""
 __all__ = ["variable_rolling_mean", "circular_convolution"]
 
 from typing import Iterable
-import numpy as np
 import warnings
+import numpy as np
 
 
-def variable_rolling_mean(array: Iterable[float], kernel_size: int) -> np.ndarray:
+def variable_rolling_mean(
+    array: Iterable[float], kernel_size: int, suppress_warning: bool = False
+) -> np.ndarray:
     """Apply rolling average convolution with variable kernel size
     at the beginning and end, leading to no boundary effect"""
+    if kernel_size == 0:
+        if not suppress_warning:
+            warnings.warn("Don't fool me")
+        return array
+
+    if len(array) == 0:
+        if not suppress_warning:
+            warnings.warn("Got empty array, returning empty array")
+        return array
+
     if kernel_size > len(array):
         kernel_size = len(array)
+        if not suppress_warning:
+            warnings.warn(
+                "Kernel size < array length, so kernel size is now set to array length"
+            )
 
     convolved = np.convolve(
         np.array(array, dtype=float),
@@ -23,10 +40,14 @@ def variable_rolling_mean(array: Iterable[float], kernel_size: int) -> np.ndarra
     return convolved
 
 
-def circular_convolution(array: Iterable[float], kernel: Iterable[float]) -> np.ndarray:
-    kernel_size = kernel.shape[0]
+def circular_convolution(
+    array: Iterable[float], kernel: Iterable[float], suppress_warning: bool = False
+) -> np.ndarray:
+    """Apply convolution in a circle, where the start and end of the array is stitched
+    together, resulting in no boundary effect."""
+    kernel_size = len(kernel)
 
-    if kernel_size % 2 == 0:
+    if kernel_size % 2 == 0 and not suppress_warning:
         warnings.warn("Convolution kernel size is even, so result is shifted.")
 
     tip = np.concatenate([array[-(kernel_size - 1) :], array[: kernel_size - 1]])
