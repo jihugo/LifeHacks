934926e56f302c34434812950a5b515d35f737cc
jihugo <yugeji2@illinois.edu>
2023-03-26 19:36:03 -0500

    add float_to_bin with simple mode

--- tools/binary_visuals.py
+++ tools/binary_visuals.py
@@ -1,6 +1,40 @@
 """Visuals for binary operations"""
 # pylint:disable=invalid-name
+import math
 import re
+
+
+def float_to_bin(fp: float, mode: str = "simple") -> str:
+    """Convert floating point to 32-bit binary with IEEE-754 standard
+
+    Parameters
+    ----------
+    fp : float
+    mode : str, default = simple
+        "simple" returns just the binary string. "tuple" returns (S, E, M),
+        "full" returns explanation
+    """
+
+    sign_int = int(fp < 0)
+
+    if fp == 0.0:
+        exponent_str = 8 * "0"
+        mantissa_str = 23 * "0"
+    else:
+        exponent_int = int(math.log2(abs(fp)))
+        exponent_str = re.split("b", bin(exponent_int + 127))[1]
+        exponent_str = (8 - len(exponent_str)) * "0" + exponent_str
+
+        mantissa_float = fp / (-1) ** sign_int / 2**exponent_int - 1
+
+        mantissa_str = ""
+        for digit_idx in range(23):
+            bit = int(mantissa_float // (1 / 2 ** (digit_idx + 1)))
+            mantissa_str += str(bit)
+            mantissa_float -= bit * (1 / 2 ** (digit_idx + 1))
+
+    if mode == "simple":
+        return f"{sign_int}{exponent_str}{mantissa_str}"
 
 
 def add(a: int | float, b: int | float) -> str:
