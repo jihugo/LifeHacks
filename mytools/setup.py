f6245c2ced4f02de9376f8c33c6a5f87ade9ceef
jihugo <yugeji2@illinois.edu>
2023-05-12 09:17:15 -0500

    rename and make package installable

---
+++ mytools/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+setup(
+    name="mytools",
+    version="1.0",
+    packages=parse_requirements("requirements.txt")
+)
