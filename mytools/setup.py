04e6500cb5c561558fbdea25d7dcd3c3ed0222b2
jihugo <yugeji2@illinois.edu>
2023-05-12 09:20:27 -0500

    lint

--- mytools/setup.py
+++ mytools/setup.py
@@ -1,12 +1,11 @@
+"""Setup file for mytools packages"""
 from setuptools import setup
 
+
 def parse_requirements(filename):
-    """ load requirements from a pip requirements file """
-    lineiter = (line.strip() for line in open(filename))
+    """load requirements from a pip requirements file"""
+    lineiter = (line.strip() for line in open(filename, encoding="UTF8"))
     return [line for line in lineiter if line and not line.startswith("#")]
 
-setup(
-    name="mytools",
-    version="1.0",
-    packages=parse_requirements("requirements.txt")
-)
+
+setup(name="mytools", version="1.0", packages=parse_requirements("requirements.txt"))
